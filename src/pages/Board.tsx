179aff94681ce92820ab37622f9804fbaae28840
dev <dev@dev.io>
2019-10-12 10:49:38 +0900

    revert.

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -4,44 +4,39 @@ import ImageUtil from "../utils/ImageUtil";
 import whiteboardImage from "../images/whiteboard.png";
 import kokubanImage from "../images/kokuban.png";
 import Loading from "../components/Loading";
-import { store, view } from "react-easy-state";
 
-const initialState = {
-  text: `ほら、
-・こんな
+type Props = {
+  type?: "white" | "black";
+};
+
+export default (props: Props = { type: "black" }) => {
+  const { type } = props;
+  const maxRow = type === "white" ? 10 : 8;
+  const initialState = {
+    text: `ほらね
+  ・こんな
 　・かんじで
 　・かんたんに
 　・リアルな
 　・もじが
 
 かけますよ。`
-};
-
-const globalState = store(initialState);
-const changeText = (afterText: string) => (globalState.text = afterText);
-
-type Props = {
-  type?: "white" | "black";
-};
-
-const Board = (props: Props = { type: "black" }) => {
-  const { type } = props;
-  const maxRow = type === "white" ? 10 : 8;
-
+  };
   const targetRef = useRef(null);
+  const [text, setText] = useState<string>(initialState.text);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleTextKeyDown = (
     event: React.KeyboardEvent<HTMLTextAreaElement>
   ) => {
-    const rows = globalState.text.split("\n").length;
+    const rows = text.split("\n").length;
     if (event.keyCode === 13 && rows >= maxRow) {
       event.preventDefault();
     }
   };
 
   const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    changeText(event.target.value);
+    setText(event.target.value);
   };
 
   const handleSave = () => {
@@ -69,7 +64,7 @@ const Board = (props: Props = { type: "black" }) => {
         }}
       >
         <textarea
-          value={globalState.text}
+          value={text}
           onChange={handleTextChange}
           onKeyDown={handleTextKeyDown}
           style={{
@@ -92,5 +87,3 @@ const Board = (props: Props = { type: "black" }) => {
     </>
   );
 };
-
-export default view(Board);
