9cfa148af1c51fe897faea32b29d4e605864a7aa
dev <dev@dev.io>
2020-03-28 15:48:44 +0900

    update.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import SlackIcon from "./SlackIcon";
 import Board from "./Board";
+import SlackIcon from "./SlackIcon";
 import packageJson from "../../package.json";
 
 export default () => {
+  const tabs = [
+    { title: "Slackアイコン", component: <SlackIcon /> },
+    { title: "黒板", component: <Board type="black" /> },
+    { title: "ホワイトボード", component: <Board type="white" /> },
+    { title: "Info", component: <div>バージョン：{packageJson.version}</div> }
+  ];
+  const dispTabList = () => {
+    const _children: JSX.Element[] = [];
+    tabs.forEach(item => {
+      _children.push(<Tab>{item.title}</Tab>);
+    });
+    return <TabList>{_children}</TabList>;
+  };
+  const dispTabPanels = () => {
+    const _children: JSX.Element[] = [];
+    tabs.forEach(item => {
+      _children.push(<TabPanel>{item.component}</TabPanel>);
+    });
+    return _children;
+  };
   return (
     <Tabs>
-      <TabList>
-        <Tab>黒板</Tab>
-        <Tab>ホワイトボード</Tab>
-        <Tab>Slack Icon</Tab>
-        <Tab>Info</Tab>
-      </TabList>
-
-      <TabPanel>
-        <Board type="black" />
-      </TabPanel>
-      <TabPanel>
-        <Board type="white" />
-      </TabPanel>
-      <TabPanel>
-        <SlackIcon />
-      </TabPanel>
-      <TabPanel>
-        <div>バージョン：{packageJson.version}</div>
-      </TabPanel>
+      {dispTabList()}
+      {dispTabPanels()}
     </Tabs>
   );
 };
---
+++ src/pages/Rpg.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from "react";
+import fonts from "../fonts";
+import "../fonts/index.css";
+import ImageUtil from "../utils/ImageUtil";
+
+export default () => {
+  const targetRef = useRef(null);
+  const [text, setText] = useState<string>("こんちわ");
+  const [fontName, setFontName] = useState("game");
+  const containerSize = 128;
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFontName(event.target.value);
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  const handleSave = () => {
+    ImageUtil.saveAsPng(targetRef);
+  };
+
+  const displaySelectOptions = () => {
+    let fontArray = [];
+    for (const key in fonts) {
+      if (fonts.hasOwnProperty(key)) {
+        fontArray.push(Object.assign({}, fonts[key], { name: key }));
+      }
+    }
+    return fontArray.map(item => {
+      return <option value={item.name}>{item.title}</option>;
+    });
+  };
+
+  const currentFont = fonts[fontName];
+
+  return (
+    <>
+      <div>
+        <input type="text" value={text} onChange={handleTextChange} autoFocus />
+        <select onChange={handleSelectChange}>{displaySelectOptions()}</select>
+      </div>
+      <div
+        ref={targetRef}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: `${containerSize}px`,
+          height: `${containerSize}px`,
+          padding: `${currentFont.padding}px`,
+          fontFamily: `${fontName}`,
+          fontSize: `${currentFont.fontSize}px`,
+          lineHeight: `${currentFont.fontSize}px`,
+          borderWidth: `${currentFont.borderWidth}px`,
+          borderColor: `${currentFont.borderColor}`,
+          backgroundColor: `${currentFont.backgroundColor}`,
+          color: `${currentFont.color}`,
+          borderStyle: "solid",
+          borderRadius: "20px",
+          overflow: "hidden",
+          boxSizing: "border-box"
+        }}
+      >
+        {text}
+      </div>
+      <input type="button" value="Save" onClick={handleSave} />
+    </>
+  );
+};
